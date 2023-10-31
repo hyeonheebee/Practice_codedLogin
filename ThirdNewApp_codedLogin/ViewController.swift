dc5c02092146ad0b4ffb862ef8aef9bfbcc0d955
Hyeonhee <hyunheeshim0103@gmail.com>
2023-10-31 18:32:45 +0900

    이메일 텍스트필드 뷰 설정(불필요한 코드 삭제)

--- ThirdNewApp_codedLogin/ViewController.swift
+++ ThirdNewApp_codedLogin/ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController {
         view.layer.cornerRadius = 8
         view.clipsToBounds = true
         view.backgroundColor = UIColor.darkGray
-        view.addSubview(emailTextFieldView)    // 메모리에 있는 emailTextFieldView 을 실제로 화면에 보여지도록 함(view에 표시)
+        view.addSubview(emailTextField)
+        view.addSubview(emailInfoLabel)
+     // 메모리에 있는 emailTextFieldView 을 실제로 화면에 보여지도록 함(view에 표시)
      // 이미 UIViewController 에는 view라는 속성이 있음
      //  emailTextFieldView를 let이 아닌 lazy var 로 설정하면 추가 가능한 코드, view가 먼저 생성되야 하므로 emailTextFieldView를 나중에 생성될 수 있게 하기 위함
         return view
@@ -138,25 +140,14 @@ class ViewController: UIViewController {
     
     func makeUI() {
         // 💡주의! 코드로 오토레이아웃 셋팅을 위한 사전 셋팅(필수) => 코드로 UI작성시 자동으로 frame 기준으로 오토레이아웃으로 잡아주는 기능을 끄는 코드
-        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+        emailInfoLabel.translatesAutoresizingMaskIntoConstraints = false
         
         // 오토레이아웃 셋팅(add New Constraints 했던 부분을 코드로 구현) 기준점이 필요
         // 왼쪽 앵커 잡기(닻 내리기, 기준점에서 왼쪽이 얼마만큼)
-        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        // isActive = true까지 해줘야 설정이 마무리 되는 구조
-        
-        // 오른쪽 앵커 잡기(닻 내리기, 기준점에서 오른쪽이 얼마만큼)
-        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        // -30 임에 유의
-        
-        // 위쪽 앵커 잡기(닻 내리기, 기준점에서 위에가 얼마만큼)
-        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
-        
-        // 높이 설정
-        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        // 기준점이 없으므로 equlToConstant 파라미터 메서드 사용
-        
-
+        emailInfoLabel.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 8).isActive = true
+        emailInfoLabel.trailingAnchor.constraint(equalTo: emailTextFieldView.trailingAnchor, constant: 8).isActive = true
+        emailInfoLabel.centerYAnchor.constraint(equalTo: emailTextFieldView.centerYAnchor).isActive = true
+ 
         
     }
 
