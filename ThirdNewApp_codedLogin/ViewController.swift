db259b9f8f77dec288bb90496ef36d39522bfde1
Hyeonhee <hyunheeshim0103@gmail.com>
2023-10-31 16:01:49 +0900

    Initial Commit

---
+++ ThirdNewApp_codedLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdNewApp_codedLogin
+//
+//  Created by 심현희 on 2023/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
